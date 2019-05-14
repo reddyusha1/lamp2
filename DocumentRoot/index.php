9a3591bd2bc5d9e549a8b98c5cd5681df9591339
reddyusha1 <gsaui2810@gmail.com>
2019-05-14 14:36:17 +0530

    changed index file

--- DocumentRoot/index.php
+++ DocumentRoot/index.php
@@ -1,21 +1,46 @@
+<!-- File: server-variables.php -->
+
+<html>
+
 <head>
-<title>PHP Hello Example</title>
+<title>ServerVariables Example</title>
+<link rel="stylesheet" type="text/css" href="../styles.css" />
 </head>
 
 <body>
 
-<h1>PHP Hello Example</h1>
+<h2>ServerVariables Example</h2>
 
-<p>
-<?php
-    echo "Hello\n";
-    echo "From the PHP Server\n";
+<p>Print the values of server variables.</p>
+
+<form action="server-variables.php" method="get">
+<?php 
+    if (!isset($_GET["is_submitted"]))
+    {
+        echo <<<HTML
+<input type="submit" name="btnSubmit" value="Show Server Variables" />
+<input type="hidden" name="is_submitted" value="1" />
+HTML;
+    }
+    else
+    {
+        echo <<<HTML
+<table border="1" cellpadding="5">
+<tr><th>Variable</th><th>Value</th> </tr>
+<tr><td>PHP_SELF</td>       <td>$_SERVER[PHP_SELF]</td>       </tr>
+<tr><td>QUERY_STRING</td>   <td>$_SERVER[QUERY_STRING]</td>   </tr>
+<tr><td>SERVER_NAME</td>    <td>$_SERVER[SERVER_NAME]</td>    </tr>
+<tr><td>DOCUMENT_ROOT</td>  <td>$_SERVER[DOCUMENT_ROOT]</td>  </tr>
+<tr><td>REMOTE_ADDR</td>    <td>$_SERVER[REMOTE_ADDR]</td>    </tr>
+<tr><td>HTTP_REFERER</td>   <td>$_SERVER[HTTP_REFERER]</td>   </tr>
+<tr><td>HTTP_USER_AGENT</td><td>$_SERVER[HTTP_USER_AGENT]</td></tr>
+</table>
+
+<input type="hidden" name="is_submitted" value="1" />
+HTML;
+    }
 ?>
-</p>
-
+</form>
 </body>
-
 </html>
-
-
 
