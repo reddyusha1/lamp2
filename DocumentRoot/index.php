f8c7d7703e42198676e8d24fbb9da82ebe80bbcb
reddyusha1 <gsaui2810@gmail.com>
2019-05-14 13:10:49 +0530

    my first commit index.php file

---
+++ DocumentRoot/index.php
@@ -0,0 +1,21 @@
+<head>
+<title>PHP Hello Example</title>
+</head>
+
+<body>
+
+<h1>PHP Hello world</h1>
+
+<p>
+<?php
+    echo "Hello\n";
+    echo "From the PHP Server\n";
+?>
+</p>
+
+</body>
+
+</html>
+
+
+
